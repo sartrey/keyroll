f043ac48481c4b3458edeb848dfab081333e236b
Sartrey Lee <sartrey@live.cn>
2014-07-29 10:40:03 +0800

    提交20140729 0
    使用MahApps.Metro

---
+++ Keyroll/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.Controls;
+
+namespace Keyroll
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
