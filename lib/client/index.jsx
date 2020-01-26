570eccfcd162ac08512edf4a91047375d90fe880
sartrey <sartrey@163.com>
2020-01-26 14:29:58 +0800

    support fields edit

--- lib/client/index.jsx
+++ lib/client/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'whatwg-fetch';
 import { Layout, Button, Menu, Icon, Modal, Input } from 'antd';
 import NewRecordModal from './component/NewRecordModal';
+import NewFieldModal from './component/NewFieldModal';
 import Record from '../shared/record';
 import './index.scss';
 
@@ -14,9 +15,11 @@ export default class extends Component {
       model: {
         devices: {},
         records: [],
+        current: null,
       },
       modal: {
-        'new-record': false
+        'new-record': false,
+        'new-field': false
       },
     };
   }
@@ -39,6 +42,24 @@ export default class extends Component {
     });
   }
 
+  loadRecord(query) {
+    let apiPath = '/__/data/loadRecord';
+    return fetch(apiPath)
+    .then(response => response.json())
+    .then(json => {
+      if (json.state) {
+        const { model } = this.state;
+        const i = model.records.findIndex(e => e.domain === query.domain);
+        if (i < 0) {
+          model.records.push(json.model);
+        } else {
+          model.records.splice(i, 1, json.model);
+        }
+        this.setState({ model });
+      }
+    });
+  }
+
   loadRecords(query) {
     let apiPath = '/__/data/loadRecords'
     return fetch(apiPath)
@@ -47,6 +68,8 @@ export default class extends Component {
       if (json.state) {
         const { model } = this.state;
         model.records = json.model;
+        console.log(model.records);
+        model.current = model.records[0];
         this.setState({ model });
       }
     })
@@ -56,7 +79,7 @@ export default class extends Component {
     let apiPath = '/__/data/saveRecord';
     return fetch(apiPath, {
       method: 'POST',
-      body: JSON.stringify(record.toJSONObject())
+      body: JSON.stringify(record)
     })
     .then(response => response.json())
     .then(json => {
@@ -65,14 +88,39 @@ export default class extends Component {
     });
   }
 
+  saveRecordField(record) {
+    console.log(record);
+    let apiPath = '/__/data/saveRecordField';
+    return fetch(apiPath, {
+      method: 'POST',
+      body: JSON.stringify(record)
+    })
+    .then(response => response.json())
+    .then(json => {
+      this.loadRecord();
+    });
+  }
+
+  killRecordField() {
+  }
+
   submitInput(type, data) {
-    const { modal } = this.state;
-    const record = new Record(data);
+    const { model, modal } = this.state;
     if (type === 'new-record') {
-      this.saveRecord(record)
+      this.saveRecord(data)
         .then(() => {
-          this.loadRecords(record);
+          this.loadRecords();
           modal['new-record'] = false;
+          this.setState({ modal });
+        });
+    }
+    if (type === 'new-field') {
+      const record = model.current;
+      record.fields.push(data);
+      this.saveRecordField(record)
+        .then(() => {
+          this.loadRecord({ domain: record.domain });
+          modal['new-field'] = false;
           this.setState({ modal });
         });
     }
@@ -113,8 +161,8 @@ export default class extends Component {
         </div>
         <ul>
           {records.map((record, i) => (
-            <li className='file-item' key={i}>
-              <a onClick={e => this.openModal('file-info', record)}>{record.domain}</a>
+            <li className='record-item' key={record}>
+              <a onClick={e => e}>{record.domain}</a>
             </li>
           ))}
         </ul>
@@ -123,25 +171,27 @@ export default class extends Component {
     );
   }
   
-  // renderContent() {
-  //   const { query, model } = this.state;
-  //   const { keyword } = query;
-  //   const { records } = model;
-  //   if (keyword) {
-  //     records = records.filter(record => record.domain.indexOf(keyword) >= 0)
-  //   }
-  //   return (
-  //     <Panel name='content' title='Content' actions={[]}>
-  //       <ul>
-  //         {records.map((record, i) => (
-  //           <li className='file-item' key={i}>
-  //             <a onClick={e => this.openModal('file-info', record)}>{record.domain}</a>
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     </Panel>
-  //   );
-  // }
+  renderFields() {
+    const { query, model, modal } = this.state;
+    const { current } = model;
+    return (
+      <div>
+        <div className="control-box">
+          <Button type="primary" icon="plus-square" onClick={e => this.showModal('new-field')}>New Field</Button>
+        </div>
+        <ul>
+          {current && current.fields.map((field, i) => (
+            <li className='field-item' key={field.key}>
+              <p>{field.key}</p>
+              <p>{field.value}</p>
+              <p>{field.secure}</p>
+            </li>
+          ))}
+        </ul>
+        <NewFieldModal visible={modal['new-field']} onSubmit={input => this.submitInput('new-field', input)} />
+      </div>
+    );
+  }
 
   render() {
     return (
@@ -157,7 +207,7 @@ export default class extends Component {
             {this.renderRecords()}
           </Layout.Sider>
           <Layout.Content className="content">
-            main content
+            {this.renderFields()}
           </Layout.Content>
         </Layout>
       </Layout>
